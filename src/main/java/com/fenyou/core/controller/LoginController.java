0d1d7558ae79d5e22032e6194c5bf2119cd872e0
kinky <kinky2015vip@gmail.com>
2016-01-24 01:08:49 +0800

    +)

---
+++ src/main/java/com/fenyou/core/controller/LoginController.java
@@ -0,0 +1,5 @@
+package com.fenyou.core.controller;
+
+public class LoginController {
+
+}
---
+++ src/main/java/com/fenyou/core/entity/User.java
@@ -0,0 +1,30 @@
+package com.fenyou.core.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String userName;
+	private String passWord;
+	public User(String userName, String passWord) {
+		super();
+		this.userName = userName;
+		this.passWord = passWord;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+}
