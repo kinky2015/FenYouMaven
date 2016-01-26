03f0a5d0a7271e608c7c45f50b87e4d763246d88
kinky <kinky2015@163.com>
2016-01-26 16:02:53 +0800

    提交

--- src/main/java/com/fenyou/core/controller/LoginController.java
+++ src/main/java/com/fenyou/core/controller/LoginController.java
@@ -3,14 +3,21 @@ package com.fenyou.core.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fenyou.core.model.User;
+import com.fenyou.core.service.UserService;
+
 @Controller
+@RequestMapping("/pages")
 public class LoginController {
+	@Autowired
+	private UserService<User> userService;
 	@RequestMapping("/login.do")
 	public @ResponseBody Map<String, Object> login(@RequestParam("username") String username,
             @RequestParam("password") String password, ModelMap modelMap){
